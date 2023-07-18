be622d843cc2b995d9f4fa436156b5da7f3555df
Jodymendo Bandoles <jodymendo14@gmail.com>
2023-07-18 22:58:21 +0800

    Naming the Title to React Exercise

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container">
-        <p>&copy; 2023 Umaasa sa tala Company. All rights reserved.</p>
-        <p>Contact: example@example.com</p>
-        <p>Address: 123 sa puso mo, Pagmamahal City, Walang Country</p>
-      </div>
-    </footer>
+    <>
+        <footer className="footer">
+            <div className="container">
+                <p>&copy; 2023 Umaasa sa tala Company. All rights reserved.</p>
+                <p>Contact: example@example.com</p>
+                <p>Address: 123 sa puso mo, Pagmamahal City, Walang Country</p>
+            </div>
+        </footer>
+    </>
+    
   );
 };
 
--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
 import Card from './Card';
 
 const MainContainer = () => {
   const skills = ['HTML', 'CSS', 'JavaScript', 'ReactJS', 'Video Editing', 'Graphic Design'];
 
   return (
-    <div className="main-container">
-      <h1>Main Container</h1>
-      <div className="card-container">
-        {skills.map((skill, index) => (
-          <Card key={index} skill={skill} />
-        ))}
+    <>
+       <div className="main-container">
+          <h1>Main Container</h1>
+          <div className="card-container">
+            {skills.map((skill, index) => (
+            <Card key={index} skill={skill} />
+            ))}
+        </div>
       </div>
-    </div>
+    </>
+   
   );
 };
 
